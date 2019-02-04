4c2bcf215b5a74e22efd40d9c719af7b3df6f120
Wenlong Xiong <wenlongx@gmail.com>
2019-02-04 15:28:42 -0800

    tabs to spaces

--- Homework/HW1/HW1.py
+++ Homework/HW1/HW1.py
@@ -10,24 +10,32 @@ from torchvision import datasets, transforms
 
 ## USE THIS SNIPPET TO GET BINARY TRAIN/TEST DATA
 
-train_data = datasets.MNIST('/data/patrick-data/pytorch/data/', train=True, download=True,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ]))
-test_data = datasets.MNIST('/data/patrick-data/pytorch/data/', train=False, download=True,
-                   transform=transforms.Compose([
-                       transforms.ToTensor(),
-                       transforms.Normalize((0.1307,), (0.3081,))
-                   ]))
-subset_indices = ((train_data.train_labels == 0) + (train_data.train_labels == 1)).nonzero()
-train_loader = torch.utils.data.DataLoader(train_data,batch_size=batch_size, shuffle=False,sampler=SubsetRandomSampler(subset_indices))
+train_data = datasets.MNIST('/data/patrick-data/pytorch/data/',
+                            train=True,
+                            download=True,
+                            transform=transforms.Compose([
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.1307,), (0.3081,))
+                            ]))
+test_data = datasets.MNIST('/data/patrick-data/pytorch/data/',
+                           train=False,
+                           download=True,
+                           transform=transforms.Compose([
+                               transforms.ToTensor(),
+                               transforms.Normalize((0.1307,), (0.3081,))
+                           ]))
+subset_indices = ((train_data.train_labels == 0) +
+                  (train_data.train_labels == 1)).nonzero()
+train_loader = torch.utils.data.DataLoader(
+    train_data, batch_size=batch_size, shuffle=False,
+    sampler=SubsetRandomSampler(subset_indices))
 
 
-subset_indices = ((test_data.test_labels == 0) + (test_data.test_labels == 1)).nonzero()
-test_loader = torch.utils.data.DataLoader(test_data,batch_size=batch_size, shuffle=False,sampler=SubsetRandomSampler(subset_indices))
-
-
+subset_indices = ((test_data.test_labels == 0) +
+                  (test_data.test_labels == 1)).nonzero()
+test_loader = torch.utils.data.DataLoader(
+    test_data, batch_size=batch_size, shuffle=False,
+    sampler=SubsetRandomSampler(subset_indices))
 
 
 # Training the Model
@@ -38,11 +46,11 @@ for epoch in range(num_epochs):
     for i, (images, labels) in enumerate(train_loader):
         images = Variable(images.view(-1, 28*28))
         #Convert labels from 0,1 to -1,1
-	labels = Variable(2*(labels.float()-0.5))
+        labels = Variable(2*(labels.float()-0.5))
 
         ## TODO
 
-    #Print your results every epoch
+    # Print your results every epoch
 
 
 # Test the Model
