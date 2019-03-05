833a10e0dfba74c4a6547e0348bcc8151d8ef33e
Wenlong Xiong <wenlongx@gmail.com>
2019-03-04 16:59:29 -0800

    hw3.py

---
+++ Homework/HW3/hw3.py
@@ -0,0 +1,36 @@
+import torch
+from torch.autograd import Variable
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torch.nn.functional as F
+import numpy as np
+import torch.utils.data as td
+import random,time
+
+
+def cifar_loaders(batch_size, shuffle_test=False): 
+    normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.225, 0.225, 0.225])
+    train = datasets.CIFAR10('./', train=True, download=True, 
+        transform=transforms.Compose([
+            transforms.RandomHorizontalFlip(),
+            transforms.RandomCrop(32, 4),
+            transforms.ToTensor(),
+            normalize,
+        ]))
+    test = datasets.CIFAR10('./', train=False, 
+        transform=transforms.Compose([transforms.ToTensor(), normalize]))
+    train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size,
+        shuffle=True, pin_memory=True)
+    test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size,
+        shuffle=shuffle_test, pin_memory=True)
+    return train_loader, test_loader
+
+batch_size = 64
+test_batch_size = 64
+
+train_loader, _ = cifar_loaders(batch_size)
+_, test_loader = cifar_loaders(test_batch_size)
+
